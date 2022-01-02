094788602040c261f56e4ea6956043f4dd0c45d4
Anikó Borosová <borosanikoo@gmail.com>
2022-01-02 13:00:10 +0100

    add envConfg + prepareConfg

--- prepareConfig.js
+++ prepareConfig.js
@@ -3,10 +3,12 @@ const path = require("path");
 const nconf = require("nconf");
 
 nconf.argv().env().file({ file: './secretConfig' });
+let envConfigJson = require("./tests/testData/envConfig");
 let testDataJson = require("./tests/testData/userData");
 
-// write configs and data into separate folder
+// write env configs and test data into separate folder
 const stringedTestDataJson = JSON.stringify(testDataJson);
+const stringedEnvConfigJson = JSON.stringify(envConfigJson);
 
 let configDir = path.join(__dirname, "./configs");
 if (!fs.existsSync(configDir)) {
@@ -14,6 +16,11 @@ if (!fs.existsSync(configDir)) {
 }
 
 fs.writeFileSync(
-    path.join(configDir, "testConfig.json"),
+    path.join(configDir, "envConfig.json"),
+    stringedEnvConfigJson
+);
+
+fs.writeFileSync(
+    path.join(configDir, "testData.json"),
     stringedTestDataJson
 );
