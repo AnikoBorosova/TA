13317a0dee06526d6a4ca2c21c0f44c54a9e1526
Anikó Borosová <borosanikoo@gmail.com>
2022-02-20 19:39:48 +0100

    adjust browser version

--- wdio-github-ubuntu.conf.js
+++ wdio-github-ubuntu.conf.js
@@ -2,7 +2,7 @@ const sharedConfig = require("./wdio-shared.conf");
 
 const drivers = {
     chrome: {
-        version: '98.0.4758.102',
+        version: '97.0.4692.99',
         arch: process.arch,
         baseURL: 'https://chromedriver.storage.googleapis.com'
     },
