76133d7591ec67badbe4726b0064a695fefc1de9
aborosova <aniko.borosova@dashlane.com>
2023-05-05 10:21:12 +0100

    chore(safari-setup): tets url

--- tests2/specs/openSite.js
+++ tests2/specs/openSite.js
@@ -2,6 +2,7 @@ describe("Test for opening website", () => {
 
 	it("navigates to 'Sign in'page and checks if it opens", () => {
         browser.url('https://webdriver.io');
-        console.log('URL LOGEGD ', await browser.getUrl())
+        const url = browser.getUrl();
+        console.log('URL LOGGED ',url);
 	});
 });
